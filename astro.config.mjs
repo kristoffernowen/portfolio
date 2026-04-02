2921fd6147601774cbb5d0cc3cf701be24558c0b
Kristoffer Nowén <kristoffer.nowen@learnet.se>
2026-04-02 20:16:59 +0200

    Add base, site and output for astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://kristoffernowen.github.io",
+  base: "/portfolio/",
+  output: "static",
   integrations: [mdx()],
 });
